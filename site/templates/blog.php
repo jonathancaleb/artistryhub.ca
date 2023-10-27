33abe97bc918e97b4b484ce0a469d1bc959bea6d
jonathancaleb <jonathancaleb230@gmail.com>
2023-10-27 19:24:41 +0300

    :2nd_place_medal: internalization

--- site/templates/blog.php
+++ site/templates/blog.php
@@ -3,12 +3,12 @@
 <main class="flex flex-col gap-16">
     <section class="container max-w-screen-lg mx-auto">
         <div class="flex flex-col gap-4 px-8 mx-auto md:px-16 lg:px-0">
-            <p class="font-mono font-bold uppercase text-slate-600">Mes articles 📰</p>
+            <p class="font-mono font-bold uppercase text-slate-600">My articles 📰</p>
             <h1 class="text-4xl font-bold">
                 Blog
             </h1>
             <div class="text-lg">
-                De la <strong>vulgarisation</strong>, des tutoriels <strong>techniques</strong>, etc (voir Figma)
+            Of the <strong>popularization</strong>, tutorials<strong>techniques</strong>, etc (see Figma)
             </div>
         </div>
     </section>
